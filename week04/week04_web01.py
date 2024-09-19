bacb8ae62e4072ba31cf331bcbdd9f125da9ff4f
tt11tt22 <tt11tt22@email.daelim.ac.kr>
2024-09-19 10:49:48 +0900

    v0.9 BP2

---
+++ week04/week04_web01.py
@@ -0,0 +1,14 @@
+import urllib.request
+import urllib.parse
+
+api = "https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp"
+
+station_id = input("지역코드 : ") # 전국 108, 수도권 109, 강원 105, 제주 184...
+values = {'stnId':station_id}
+
+url = api + '?' + urllib.parse.urlencode(values)
+# print(url)
+
+urls = urllib.request.urlopen(url).read()
+texts = urls.decode('utf-8')
+print(texts)
